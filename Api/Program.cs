325d1530807bf1d0f1809eed46d4261af534ef4e
oHTGo <nguyennhathuy.orit@gmail.com>
2022-05-18 11:14:20 +0700

    Add src & workflow

---
+++ Api/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Persistence.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+//Add service to DI container
+{
+    builder.Services.AddDbService(configuration);
+    builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    builder.Services.AddControllers(
+        options =>
+        {
+            options.SuppressAsyncSuffixInActionNames = false;
+        }
+    );
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+
+var app = builder.Build();
+// Add service to Http request pipeline
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseDeveloperExceptionPage();
+        await app.Services.ApplyMigrations();
+    }
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+
